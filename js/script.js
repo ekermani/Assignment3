4488b38731983b0b839a6c0313ad7e08531200e8
Erica Kermani <ekermani@gmail.com>
2016-09-21 23:34:54 -0400

    finalized API, added more styling

--- js/script.js
+++ js/script.js
@@ -1,48 +1,40 @@
 // hold variables (input)
-var city = '';
-var state = '';
-var options = '';
+// var name = '';
+// var place = '';
+var keyword = '';
 
 // insert all data into the divs
-function loadCollection(unicorn) {  //unicorn aka data, refers to entire object
-	console.log(unicorn);
+function loadCollection(data) {  //unicorn aka data, refers to entire object
+	console.log(data);
 
-// pulling list of data
-	if(unicorn.response.error) { //unicorn is the name of the data being passed (so it's the whole entire object in this case)
-		alert(unicorn.response.error.description);  //to access the object inside of it, you use .
-		return;
-	}
+// // pulling list of data
+// 	if(unicorn.response.error) { //unicorn is the name of the data being passed (so it's the whole entire object in this case)
+// 		alert(unicorn.response.error.description);  //to access the object inside of it, you use .
+// 		return;
+// 	}
 
 // get museum collection data (output)
-	var title = unicorn.current_observation.display_location.city;
-	var artist = unicorn.current_observation.temp_f; //if get array instead, then type [0] first number for example
-	var thumbnail = unicorn.current_observation.icon_url;
+	var title = data.records[0].fields.title;
+	var artist = data.records[0].fields.artist; 
+	var thumbnail = data.records[0].fields.primary_image_id; 
 
 	console.log(title);
 	console.log(artist);
 	console.log(thumbnail);
 
-	$('#temperature').html(temperature); //.html changes html inside tags
-	$('#weather').html(weather);
-	$('#weatherIcon').html('<img src=" '+ icon +'">');
-
-	// if(thisCity == 'New York') {  //make comparisons //leave blank to clear
-	// 	$('#currentCity').val("New Rocks Yo!"); 
-	// } else {
-	// 	$('#currentCity').val("This city sucks");
-	// }
+	$('#image').html('<img src=http://media.vam.ac.uk/media/thira/collection_images/' + thumbnail.slice(0,6) +'/' + thumbnail + '_jpg_ds.jpg>');
+	$('#title').html('Title: ' + title);
+	$('#artist').html('Artist: ' + artist);
 
 }
 
 
 // get museum collection data
 function getCollection() {
-	var myUrl = "http://www.vam.ac.uk/api/json/museumobject/", options;
-	// after json is format of response
-	var myUrl = "http://api.wunderground.com/api/" + APIkey + "/conditions/q/" + state +"/" + city + ".json";
+	var myUrl = "http://www.vam.ac.uk/api/json/museumobject/search?q=" + keyword;
 
 
-	$.ajax({ //calls to server, loads json file //changed ajax to getJSON
+	$.ajax({ //calls to server, loads json file
 		url: myUrl,
 		dataType: "jsonp",
 		success: function(response) { //passes response into load weather function
@@ -60,31 +52,22 @@ function getCollection() {
 // pass museum collection data when submit button clicked
 // pull value from input 
 function setCollection() {
-	city = $('#currentCity').val(); //get value from currentCity input
-	state = $('#currentState').val(); //get value from currentState input
+	keyword = $('#q').val(); //get value from currentCity input
 
-	if(city == null || city =='' ) {
-		alert('Enter keywords to search the musuem collection');
-	}
-
-	console.log("Museum Object " + city + ", " + state);
+	console.log("Museum Object " + keyword + " ");
 	getCollection();
-
 }
-
 
 // initialize
 function init() {
-	console.log("Hello");
-
 	$('#submit').click(function(e){
 		e.preventDefault();
+		// console.log("works");
 		setCollection();  //none of the functions above run until hit submit button
+
 	});
 };
 // run on load
 $(document).ready(function(){
 	init();
 });
-
-// 
